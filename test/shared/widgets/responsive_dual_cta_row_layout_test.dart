faa383117e591a35928789d6de093366579cbd33
ilker <ilkersevim2007@gmail.com>
2026-05-21 15:14:01 +0300

    fix(tests): reorder import statements in responsive dual CTA row layout test

--- test/shared/widgets/responsive_dual_cta_row_layout_test.dart
+++ test/shared/widgets/responsive_dual_cta_row_layout_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc_app/shared/widgets/responsive_action_bar.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import '../../helpers/layout_overflow_expectations.dart';
 
